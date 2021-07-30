1fe553eabe5ba75059f1caeb665c5c32d338ba03
Marco <ma.souza.junior@gmail.com>
2021-07-30 20:27:33 -0300

    fix: reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from "next/app";
 
 import Layout from "@packages/components/Layout";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import Metadata from "@packages/components/Metadata";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Layout>
+        <CSSReset />
         <Metadata />
         <Component {...pageProps} />
       </Layout>
