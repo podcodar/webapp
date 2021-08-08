b591725ec91cf575b044aefcad2e4562bb398777
Marco <ma.souza.junior@gmail.com>
2021-08-08 09:47:58 -0300

    fea: add iframe with closing state
    - add loading
    - add animation
    - add is open state
    - add iframe to form
    - create functions to handle load and close

--- packages/components/dialogs/CallToActionModal.tsx
+++ packages/components/dialogs/CallToActionModal.tsx
@@ -1,37 +1,74 @@
 import {
-  Button,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
-  useDisclosure,
+  Spinner,
+  VStack,
+  Flex,
+  SlideFade,
 } from '@chakra-ui/react';
 import {
   useModalActions,
   useModalStates,
 } from '@packages/features/modal-context';
+import React, { useState } from 'react';
 
 function CallToActionModal() {
   const { isOpen } = useModalStates();
   const { close } = useModalActions();
+  const [isIframeOpen, setIsIframeOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function handleClose() {
+    setIsIframeOpen(false);
+    close();
+    setIsLoading(true);
+  }
+
+  function handleLoad() {
+    setIsLoading(!isLoading);
+    if (!isIframeOpen) {
+      setIsIframeOpen(true);
+    } else handleClose(); // TODO: show a success modal
+  }
+
   return (
-    <Modal isOpen={isOpen} onClose={close}>
+    <Modal isOpen={isOpen} onClose={handleClose} size="md">
       <ModalOverlay />
 
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader borderBottom="1px solid" borderColor="whitesmoke">
+          Faça Parte
+        </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>Modal Body</ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={close}>
-            Close
-          </Button>
-          <Button variant="ghost">Secondary Action</Button>
-        </ModalFooter>
+        <ModalBody
+          maxH={{ base: '100%', md: '500' }}
+          d="flex"
+          justifyContent="center"
+          overflow="hidden"
+        >
+          <VStack>
+            {isLoading && (
+              <Flex alignItems="center" justifyContent="center" p="30% 0">
+                <Spinner size="lg" m="auto" color="purple.300" />
+              </Flex>
+            )}
+            <SlideFade in={isIframeOpen} offsetY="1rem">
+              <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSeoy7Jg_LaXsJMDYJ1gXKBRPu4tIdQiPBG5ZmLennVKSb_GVg/viewform?embedded=true"
+                width="400"
+                height="480"
+                frameBorder="0"
+                marginHeight={0}
+                marginWidth={0}
+                onLoad={handleLoad}
+              />
+            </SlideFade>
+          </VStack>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
