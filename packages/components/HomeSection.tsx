765bc41947b8ab4d57e3d1bdc4c26a9b2b03b72d
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-08-11 14:40:42 -0300

    create HomeSection comp and import SimpleGrid with base structure of the comp

---
+++ packages/components/HomeSection.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SimpleGrid } from '@chakra-ui/react';
+
+const HomeSection = () => {
+  return (
+    <SimpleGrid minChildWidth="150px" spacing="50px" textAlign="center">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum et
+        voluptates aperiam ipsum enim provident iusto at corporis saepe
+        officiis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia assumenda
+        totam earum aut eius quasi quam iusto suscipit et hic voluptatibus
+        ratione, cum ad quibusdam est dolores accusamus magnam tempore
+        repellendus vero non dignissimos, sint quis. Corrupti eum explicabo vero
+        alias maxime, debitis ipsum quia deleniti, voluptatem ab impedit vitae!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus deserunt
+        ab sapiente, adipisci temporibus quasi fugiat cumque iste maxime
+        dignissimos ipsa aliquid omnis, sint quam nulla dolore voluptatem
+        aspernatur necessitatibus.
+      </p>
+    </SimpleGrid>
+  );
+};
+
+export default HomeSection;
