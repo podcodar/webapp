3afd5ce9fb401cb6ebe441029debadb6848de32d
Marco <ma.souza.junior@gmail.com>
2021-08-12 17:07:02 -0300

    enh: make modal form responsive

--- packages/components/dialogs/CallToActionModal.tsx
+++ packages/components/dialogs/CallToActionModal.tsx
@@ -6,9 +6,10 @@ import {
   ModalHeader,
   ModalOverlay,
   Spinner,
-  VStack,
+  Stack,
   Flex,
   SlideFade,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import {
   useModalActions,
@@ -16,11 +17,18 @@ import {
 } from '@packages/features/modal-context';
 import React, { useState } from 'react';
 
-function CallToActionModal() {
+type ModalVariants = 'md' | 'full' | 'lg' | 'xl';
+
+export default function CallToActionModal() {
   const { isOpen } = useModalStates();
   const { close } = useModalActions();
   const [isIframeOpen, setIsIframeOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const responsiveModalSize = useBreakpointValue<ModalVariants>({
+    base: 'full',
+    md: 'md',
+    lg: 'xl',
+  });
 
   function handleClose() {
     setIsIframeOpen(false);
@@ -36,42 +44,55 @@ function CallToActionModal() {
   }
 
   return (
-    <Modal isOpen={isOpen} onClose={handleClose} size="md">
+    <Modal isOpen={isOpen} onClose={handleClose} size={responsiveModalSize}>
       <ModalOverlay />
 
-      <ModalContent>
+      <ModalContent m={useBreakpointValue({ base: '0', md: '3xl' })}>
         <ModalHeader borderBottom="1px solid" borderColor="whitesmoke">
           Faça Parte
         </ModalHeader>
         <ModalCloseButton />
-        <ModalBody
-          maxH={{ base: '100%', md: '500' }}
-          d="flex"
-          justifyContent="center"
-          overflow="hidden"
-        >
-          <VStack>
+        <ModalBody d="flex" justifyContent="center" overflow="hidden">
+          <Stack>
             {isLoading && (
-              <Flex alignItems="center" justifyContent="center" p="30% 0">
+              <Flex alignItems="center" justifyContent="center" p="10rem 0">
                 <Spinner size="lg" m="auto" color="purple.300" />
               </Flex>
             )}
-            <SlideFade in={isIframeOpen} offsetY="1rem">
-              <iframe
-                src="https://docs.google.com/forms/d/e/1FAIpQLSeoy7Jg_LaXsJMDYJ1gXKBRPu4tIdQiPBG5ZmLennVKSb_GVg/viewform?embedded=true"
-                width="400"
-                height="480"
-                frameBorder="0"
-                marginHeight={0}
-                marginWidth={0}
-                onLoad={handleLoad}
-              />
-            </SlideFade>
-          </VStack>
+
+            <GoogleForm handleLoad={handleLoad} isIframeOpen={isIframeOpen} />
+          </Stack>
         </ModalBody>
       </ModalContent>
     </Modal>
   );
 }
 
-export default CallToActionModal;
+function GoogleForm({ isIframeOpen = false, handleLoad = () => {} }) {
+  const padding = 60;
+  const pageWidth = window?.innerWidth ?? 0;
+  const pageHeight = window?.innerHeight ?? 0;
+  const width = useBreakpointValue({
+    base: pageWidth - padding,
+    md: isIframeOpen ? 400 : 0,
+    lg: isIframeOpen ? 580 : 0,
+  });
+  const height = useBreakpointValue({
+    base: pageHeight - padding,
+    md: isIframeOpen ? 480 : 0,
+    lg: isIframeOpen ? 600 : 0,
+  });
+  return (
+    <SlideFade in={isIframeOpen} offsetY="1rem">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeoy7Jg_LaXsJMDYJ1gXKBRPu4tIdQiPBG5ZmLennVKSb_GVg/viewform?embedded=true"
+        width={width}
+        height={height}
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+        onLoad={handleLoad}
+      />
+    </SlideFade>
+  );
+}
