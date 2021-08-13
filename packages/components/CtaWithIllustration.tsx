78192e1b1646a92d35f4628bf5a25e8f1f7556ae
Marco <ma.souza.junior@gmail.com>
2021-08-13 17:13:11 -0300

    enh: move container to layout

--- packages/components/CtaWithIllustration.tsx
+++ packages/components/CtaWithIllustration.tsx
@@ -1,59 +1,50 @@
-import {
-  Flex,
-  Container,
-  Heading,
-  Stack,
-  Text,
-  Button,
-} from '@chakra-ui/react';
+import { Flex, Heading, Stack, Text, Button } from '@chakra-ui/react';
 import { useModalActions } from '@packages/features/modal-context';
 import { Illustration } from './icons';
 
 export default function CallToActionWithIllustration() {
   const { open } = useModalActions();
   return (
-    <Container maxW={'5xl'}>
-      <Stack
-        textAlign={'center'}
-        align={'center'}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}
+    <Stack
+      textAlign={'center'}
+      align={'center'}
+      spacing={{ base: 8, md: 10 }}
+      py={{ base: 20, md: 28 }}
+    >
+      <Heading
+        fontWeight={600}
+        fontSize={{ base: '3xl', sm: '4xl' }}
+        lineHeight={'110%'}
       >
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl' }}
-          lineHeight={'110%'}
-        >
-          Formação profissional para o{' '}
-          <Text as={'span'} color={'purple.400'}>
-            mercado de tecnologia.
-          </Text>
-        </Heading>
-        <Text color={'gray.500'} maxW={'3xl'}>
-          Somos uma comunidade de tecnologia e computação que ensina programação
-          com foco na formação de profissionais. Existimos para democratizar o
-          conhecimento e o acesso às oportunidades de trabalho na área de
-          tecnologia.
+        Formação profissional para o{' '}
+        <Text as={'span'} color={'purple.400'}>
+          mercado de tecnologia.
         </Text>
-        <Stack spacing={6} direction={'row'}>
-          <Button
-            rounded={'full'}
-            px={6}
-            colorScheme={'purple'}
-            bg={'purple.400'}
-            _hover={{ bg: 'purple.500' }}
-            onClick={open}
-          >
-            Faça parte!
-          </Button>
-          <Button rounded={'full'} px={6}>
-            Como funciona?
-          </Button>
-        </Stack>
-        <Flex w={'full'}>
-          <Illustration height={{ sm: '24rem', lg: '28rem' }} />
-        </Flex>
+      </Heading>
+      <Text color={'gray.500'} maxW={'3xl'}>
+        Somos uma comunidade de tecnologia e computação que ensina programação
+        com foco na formação de profissionais. Existimos para democratizar o
+        conhecimento e o acesso às oportunidades de trabalho na área de
+        tecnologia.
+      </Text>
+      <Stack spacing={6} direction={'row'}>
+        <Button
+          rounded={'full'}
+          px={6}
+          colorScheme={'purple'}
+          bg={'purple.400'}
+          _hover={{ bg: 'purple.500' }}
+          onClick={open}
+        >
+          Faça parte!
+        </Button>
+        <Button rounded={'full'} px={6}>
+          Como funciona?
+        </Button>
       </Stack>
-    </Container>
+      <Flex w={'full'}>
+        <Illustration height={{ sm: '24rem', lg: '28rem' }} />
+      </Flex>
+    </Stack>
   );
 }
