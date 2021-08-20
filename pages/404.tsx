94efff2b9878e95cf7520158fd5c2ba7c64b27e9
frattezi <pfrattezi@gmail.com>
2021-08-20 09:59:56 -0300

    feat: add text message to 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Lottie from 'react-lottie';
 import * as animationData from '@packages/components/lotties/404.json';
-import { Box } from '@chakra-ui/layout';
+import { Box, Heading, Text, Stack } from '@chakra-ui/layout';
 
 const defaultOptions = {
   loop: false,
@@ -15,7 +15,28 @@ const defaultOptions = {
 export default function Home() {
   return (
     <Box>
-      <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
+      <Stack
+        textAlign={'center'}
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        pt="0.5rem"
+      >
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: '3xl', sm: '4xl' }}
+          lineHeight={'110%'}
+        >
+          Não encontramos sua página
+          <Text as={'span'} color={'purple.400'}>
+            {' '}
+            (╯°□°)╯︵ ┻━┻{' '}
+          </Text>
+        </Heading>
+        <Text color={'gray.500'} maxW={'3xl'}>
+          Mas encontramos esse abacate!
+        </Text>
+        <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
+      </Stack>
     </Box>
   );
 }
