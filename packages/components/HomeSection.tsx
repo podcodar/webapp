1b00405a8c94dec6bb0e85eb14a154939efcce31
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-08-24 11:52:49 -0300

    rebase: pls dont f this up

--- packages/components/HomeSection.tsx
+++ packages/components/HomeSection.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
+import { Icon } from '@chakra-ui/react';
+import { GoCode, GoBriefcase, GoCommentDiscussion } from 'react-icons/go';
 import { SimpleGrid } from '@chakra-ui/react';
 
 const HomeSection = () => {
   return (
     <SimpleGrid minChildWidth="150px" spacing="50px" textAlign="center">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum et
-        voluptates aperiam ipsum enim provident iusto at corporis saepe
-        officiis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia assumenda
-        totam earum aut eius quasi quam iusto suscipit et hic voluptatibus
-        ratione, cum ad quibusdam est dolores accusamus magnam tempore
-        repellendus vero non dignissimos, sint quis. Corrupti eum explicabo vero
-        alias maxime, debitis ipsum quia deleniti, voluptatem ab impedit vitae!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus deserunt
-        ab sapiente, adipisci temporibus quasi fugiat cumque iste maxime
-        dignissimos ipsa aliquid omnis, sint quam nulla dolore voluptatem
-        aspernatur necessitatibus.
-      </p>
+      <span>
+        <Icon as={GoCode} w={7} h={7} marginBottom={5} />
+        <h3>Aprenda a programar</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum et
+          voluptates aperiam ipsum enim provident iusto at corporis saepe
+          officiis.
+        </p>
+      </span>
+      <span>
+        <Icon as={GoCommentDiscussion} w={7} h={7} marginBottom={5} />
+        <h3>Aprenda a programar</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          perspiciatis neque ducimus, tempore commodi quia dolorum illum ea
+          nulla porro. Quam laborum repudiandae, similique harum eum illo et
+          minus, obcaecati nobis eos blanditiis quia dolore reiciendis aut rem
+          alias cupiditate perspiciatis quos necessitatibus qui numquam eveniet
+          repellendus. Reiciendis, atque minima.
+        </p>
+      </span>
+      <span>
+        <Icon as={GoBriefcase} w={7} h={7} marginBottom={5} />
+        <h3>Aprenda a programar</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel nobis
+          repellat, quo deserunt laboriosam odio adipisci commodi rerum beatae
+          quidem impedit, aut reiciendis eos et esse dolore eius porro numquam.
+        </p>
+      </span>
     </SimpleGrid>
   );
 };
