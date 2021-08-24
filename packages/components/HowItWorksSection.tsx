c9b08380978de21a20251bfb3398bb3e170c6b48
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-08-24 14:09:30 -0300

    add componentization of items that were repeated before

--- packages/components/HowItWorksSection.tsx
+++ packages/components/HowItWorksSection.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import { Box, useColorModeValue, Text, Heading } from '@chakra-ui/react';
-import { SimpleGrid } from '@chakra-ui/react';
+import {
+  Box,
+  useColorModeValue,
+  Text,
+  Heading,
+  Icon,
+  SimpleGrid,
+} from '@chakra-ui/react';
 import {
   CodeIcon,
   BriefcaseIcon,
@@ -8,6 +13,46 @@ import {
 } from '@packages/components/icons';
 
 const HowItWorksSection = () => {
+  const ColumnSection = () => {
+    const content = [
+      {
+        icon: ConversationIcon,
+        heading: 'Participe da comunidade',
+        text: 'Parcicipe de meetups semanais com diversos outros profissionais da área de tecnologia e expanda seus conhecimentos.',
+      },
+      {
+        icon: CodeIcon,
+        heading: 'Aprenda a programar',
+        text: 'Aprenda a programar de uma forma dinâmica através do compartilhamento de conhecimento e participe de nossas mentorias pesonalizadas ou em grupo.',
+      },
+      {
+        icon: BriefcaseIcon,
+        heading: 'Entre no mercado de trabalho',
+        text: 'Prepare-se para o mercado através de simulações de entrevistas com profissionais já consolidados na área de tecnologia.',
+      },
+    ];
+    return (
+      <>
+        {content &&
+          content.map((item) => (
+            <Box as="span" key={item.heading}>
+              <Icon
+                as={item.icon}
+                w={10}
+                h={10}
+                marginBottom="1rem"
+                color="purple.500"
+              />
+              <Heading size="sm" marginBottom="1rem">
+                {item.heading}
+              </Heading>
+              <Text color={'gray.500'}>{item.text}</Text>
+            </Box>
+          ))}
+      </>
+    );
+  };
+
   return (
     <Box
       bg={useColorModeValue('gray.50', 'gray.900')}
@@ -22,43 +67,7 @@ const HowItWorksSection = () => {
         textAlign="center"
         margin={20}
       >
-        <Text as="span">
-          <ConversationIcon
-            w={10}
-            h={10}
-            marginBottom="1rem"
-            color="purple.500"
-          />
-          <Heading size="sm" marginBottom="1rem">
-            Participe da comunidade
-          </Heading>
-          <Text color={'gray.500'}>
-            Parcicipe de meetups semanais com diversos outros profissionais da
-            área de tecnologia e expanda seus conhecimentos.
-          </Text>
-        </Text>
-        <Text as="span">
-          <CodeIcon w={10} h={10} marginBottom="1rem" color="purple.500" />
-          <Heading size="sm" marginBottom="1rem">
-            Aprenda a programar
-          </Heading>
-          <Text color={'gray.500'}>
-            Aprenda a programar de uma forma dinâmica através do
-            compartilhamento de conhecimento e participe de nossas mentorias
-            pesonalizadas ou em grupo.
-          </Text>
-        </Text>
-        <Text as="span">
-          <BriefcaseIcon w={10} h={10} marginBottom="1rem" color="purple.500" />
-
-          <Heading size="sm" marginBottom="1rem">
-            Entre no mercado de trabalho
-          </Heading>
-          <Text color={'gray.500'}>
-            Prepare-se para o mercado através de simulações de entrevistas com
-            profissionais já consolidados na área de tecnologia.
-          </Text>
-        </Text>
+        {ColumnSection()}
       </SimpleGrid>
     </Box>
   );
