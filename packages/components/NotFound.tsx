c388cc53bb143e2a95b549009a1f7bbd7a561784
frattezi <pfrattezi@gmail.com>
2021-08-25 09:10:55 -0300

    fix: lint page, use new lottie interface

--- packages/components/NotFound.tsx
+++ packages/components/NotFound.tsx
@@ -1,30 +1,29 @@
-import LottiesComponent, { AvaiableAnimations } from './LottiesComponent';
+import Lottie from '@packages/components/Lottie';
 import { Box, Heading, Text, Stack } from '@chakra-ui/layout';
 
 export default function NotFound() {
   return (
     <Box>
       <Stack
-        textAlign={'center'}
-        align={'center'}
+        textAlign="center"
+        align="center"
         spacing={{ base: 8, md: 10 }}
         pt="0.5rem"
       >
         <Heading
           fontWeight={600}
           fontSize={{ base: '3xl', sm: '4xl' }}
-          lineHeight={'110%'}
+          lineHeight="110%"
         >
           Não encontramos sua página
-          <Text as={'span'} color={'purple.400'}>
-            {' '}
-            (╯°□°)╯︵ ┻━┻{' '}
+          <Text as="span" color={'purple.400'}>
+            (╯°□°)╯︵ ┻━┻
           </Text>
         </Heading>
         <Text color={'gray.500'} maxW={'3xl'}>
           Mas encontramos esse abacate!
         </Text>
-        <LottiesComponent animation={AvaiableAnimations.avocado} />
+        <Lottie animation="avocado" />
       </Stack>
     </Box>
   );
