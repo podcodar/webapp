8cab4d7e86cdf6d8a9c54ba1e74acf6d9ca50d6f
frattezi <pfrattezi@gmail.com>
2021-08-25 09:10:25 -0300

    enh: Lottie component types and namings

---
+++ packages/components/Lottie.tsx
@@ -0,0 +1,35 @@
+import LottieComponent, { Options } from 'react-lottie';
+
+import * as avocadoAnimation from '@packages/utils/lotties/404.json';
+
+interface LottieProps {
+  animation: AvaiableAnimations;
+}
+
+export default function Lottie(props: LottieProps) {
+  const options = processOptions(props);
+
+  return (
+    <LottieComponent
+      options={options}
+      isClickToPauseDisabled={true}
+      width="70%"
+    />
+  );
+}
+
+type AvaiableAnimations = 'avocado';
+
+const lottieAnimations: Record<AvaiableAnimations, Object> = {
+  avocado: avocadoAnimation,
+};
+
+function processOptions(props: LottieProps): Options {
+  return {
+    loop: false,
+    animationData: lottieAnimations[props.animation],
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+}
