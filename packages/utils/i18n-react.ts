b7ccc8a913b3fed2e1c617a9a1064256c54a1fd3
Marco Antonio <ma.souza.junior@gmail.com>
2021-09-05 19:29:43 -0300

    fea: translate mentoring section

--- packages/utils/i18n-react.ts
+++ packages/utils/i18n-react.ts
@@ -1,5 +1,10 @@
 import { useTranslation } from 'next-i18next';
 
-type TranslationNS = 'common' | 'callToAction' | 'navbar' | 'whyItWorks';
+type TranslationNS =
+  | 'common'
+  | 'callToAction'
+  | 'navbar'
+  | 'whyItWorks'
+  | 'mentoring';
 
 export const useI18n = (ns: TranslationNS) => useTranslation(ns);
