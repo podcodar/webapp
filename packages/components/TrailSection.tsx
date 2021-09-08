d92ed3dbb1c47c0392d56c14cfc6db2af17c71d5
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-09-08 13:34:11 -0300

    enh: improve section responsiveness
    - Change section header to a Flex
    - Add Link to a 'see all' page
    - Replace Stack with Grid
    - Add a color to each Card

--- packages/components/TrailSection.tsx
+++ packages/components/TrailSection.tsx
@@ -1,4 +1,11 @@
-import { useColorModeValue, Stack, Box, Heading, Link } from '@chakra-ui/react';
+import {
+  useColorModeValue,
+  Grid,
+  Heading,
+  Link,
+  Flex,
+  Text,
+} from '@chakra-ui/react';
 
 import Section from './Section';
 
@@ -6,28 +13,33 @@ export default function TrailSection() {
   const bgColor = useColorModeValue('gray.50', 'gray.900');
   return (
     <Section bg={bgColor}>
-      <Heading
-        size="md"
-        fontWeight={400}
-        pb="2rem"
-        marginTop="2rem"
-        textAlign="center"
-      >
-        Conteúdo PodCodar
-      </Heading>
-      <Stack
-        direction={{ base: 'column', md: 'row', lg: 'row' }}
-        textAlign="center"
-        p="1rem"
-        spacing={{ base: '2rem', md: '0rem', lg: '2rem' }}
-        // bg={{base: 'red', md: 'purple', lg: 'green'}}
-        flexWrap={{ base: 'nowrap', md: 'wrap', lg: 'nowrap' }}
-        justifyContent="space-around"
+      <Flex justifyContent="space-between" py="1rem">
+        <Heading size="md" fontWeight={600} textAlign="left">
+          Conheça nossas trilhas
+        </Heading>
+        <Text textAlign="right">
+          <Link
+            href="https://www.notion.so/podcodar/Trilhas-de-estudo-eb8954febc0243b681ead5d417cca67b"
+            isExternal
+          >
+            Ver todas
+          </Link>
+        </Text>
+      </Flex>
+
+      <Grid
+        gridTemplateColumns={{
+          base: '1fr',
+          sm: 'repeat(2, 1fr)',
+          lg: 'repeat(4, 1fr)',
+        }}
+        gap="2rem"
+        my="2rem"
       >
         {cardList.map((cardProps) => (
           <CardItem key={cardProps.title} {...cardProps} />
         ))}
-      </Stack>
+      </Grid>
     </Section>
   );
 }
@@ -35,52 +47,50 @@ export default function TrailSection() {
 interface CardItemProps {
   title: string;
   link: string;
+  bgColor: string;
 }
 
-function CardItem({ title, link }: CardItemProps) {
-  const boxShadowColor = useColorModeValue('#171923', '#f7fafc');
+function CardItem({ title, link, bgColor }: CardItemProps) {
   return (
-    <Link href={link} isExternal style={{ textDecoration: 'none' }}>
-      <Box
-        w={{ base: '100%', md: '20rem', lg: '100%' }}
-        mb={{ base: '0', md: '2rem', lg: '0' }}
-        h="12rem"
-        border="1px solid"
-        transition="box-shadow 200ms ease-in-out"
-        bg={boxShadowColor === '#171923' ? '#f7fafc' : '#171923'}
+    <Link href={link} isExternal _hover={{ textDecoration: 'none' }}>
+      <Flex
+        py="2rem"
+        transition="box-shadow 300ms ease-in-out"
+        border="2px solid"
+        borderColor={bgColor}
+        borderRadius="10px"
+        justifyContent="center"
         _hover={{
-          boxShadow: `-8px 8px 0 ${boxShadowColor}`,
-          transform: 'translate(4px, -4px)',
+          boxShadow: `-8px 8px 0 ${bgColor}`,
         }}
       >
-        <Heading
-          size="md"
-          fontWeight={400}
-          color={boxShadowColor === '#171923' ? '#171923' : '#f7fafc'}
-          m="5rem 1rem 1rem 1rem"
-        >
+        <Heading size="md" fontWeight={400} textDecoration="none">
           {title}
         </Heading>
-      </Box>
+      </Flex>
     </Link>
   );
 }
 
 const cardList: CardItemProps[] = [
   {
-    title: 'Trilhas de estudo',
-    link: 'https://www.notion.so/podcodar/Trilhas-de-estudo-eb8954febc0243b681ead5d417cca67b',
+    title: 'Programação Web',
+    link: 'https://www.notion.so/podcodar/Programa-o-Web-0a244ea5a20f4b73b2c706141f7a4919',
+    bgColor: '#17A9BC',
   },
   {
-    title: 'Acervo de vídeos',
-    link: 'https://www.notion.so/podcodar/V-deos-cb62802dc021497889fb9923227c0c9a',
+    title: 'UX Design',
+    link: 'https://www.notion.so/podcodar/2dc254c4ea26420d94f400cf1275acd2?v=46a0114a307f4071be615feb0e64879f',
+    bgColor: '#F99223',
   },
   {
-    title: 'Meetups semanais',
-    link: 'https://www.notion.so/podcodar/ecda231a312e4cedac6fb36766313830?v=e6312d6a390148eda589840f335caaa8',
+    title: 'Entrar no mercado',
+    link: 'https://www.notion.so/podcodar/Entrar-no-mercado-6132c9e0873948d9943c174f66df7ee3',
+    bgColor: '#FF4CFF',
   },
   {
-    title: 'Trajetória de design',
-    link: 'https://www.notion.so/podcodar/Trajet-ria-de-Design-29a45806108d4b79aed352d67204033d',
+    title: 'Computação',
+    link: 'https://www.notion.so/podcodar/Computa-o-e7674e02bb924ea495f21575f890a831',
+    bgColor: '#b794f4',
   },
 ];
