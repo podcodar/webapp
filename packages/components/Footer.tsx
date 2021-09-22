6f0d0fa2221f332fd4c236660e119fb10a57ea6a
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-09-22 13:36:35 -0300

    fix: resolve PR comments:
    - remove key from HeaderTitle
    - delete FooterData
    - change SimpleGrid to Section
    - translate every hardcoded text

--- packages/components/Footer.tsx
+++ packages/components/Footer.tsx
@@ -1,53 +1,81 @@
-import {
-  Box,
-  SimpleGrid,
-  Stack,
-  Text,
-  useColorModeValue,
-  Container,
-} from '@chakra-ui/react';
+import { Stack, Text, useColorModeValue, Link } from '@chakra-ui/react';
 
-import { Logo } from '@packages/components/icons';
-import { useI18n } from '@packages/features/i18n-context';
-import {
-  HeaderTitle,
-  SocialLinks,
-  SupportLinks,
-  currentYear,
-} from '@packages/utils/footerData';
+import { TranslationNS, useI18n } from '@packages/features/i18n-context';
+
+import Section from './Section';
 
 export default function Footer() {
-  const bgColor = useColorModeValue('gray.200', 'gray.800');
-  const color = useColorModeValue('gray.700', 'gray.200');
   const { t } = useI18n('footer');
+  const bgColor = useColorModeValue('gray.200', 'gray.800');
+  const currentYear = new Date().getFullYear();
 
   return (
-    <Box bg={bgColor} color={color}>
-      <Container as={Stack} py="1.5rem" px="1.5rem" maxW="100%">
-        <SimpleGrid
-          flexDirection={{ base: 'column', sm: 'row' }}
-          spacing="5rem"
-          display="flex"
-          justifyContent="space-around"
-        >
-          <Stack spacing="1rem" flex="1 1 auto">
-            <Box>
-              <Logo />
-            </Box>
-            <Text fontSize="sm">
-              © {currentYear} Podcodar. {t(`legal`)}
-            </Text>
-          </Stack>
-          <Stack align="flex-start">
-            <HeaderTitle title={t(`socials`)} />
-            <SocialLinks />
-          </Stack>
-          <Stack align="flex-start">
-            <HeaderTitle title={t(`support`)} />
-            <SupportLinks />
-          </Stack>
-        </SimpleGrid>
-      </Container>
-    </Box>
+    <Section p="1.5rem" bgColor={bgColor}>
+      <Stack
+        direction={{ base: 'column', sm: 'row' }}
+        spacing={{ base: '5rem', sm: '3rem' }}
+      >
+        <Stack spacing="1rem" flex="1">
+          <Text>{t('podcodar')}</Text>
+          <Text fontSize="sm">{t(`legal`, { currentYear })}</Text>
+        </Stack>
+
+        <Stack>
+          <HeaderTitle title="socials" />
+          <LinksTransalated linksData={socialLinksData} ns="social-links" />
+        </Stack>
+
+        <Stack>
+          <HeaderTitle title="support" />
+          <LinksTransalated linksData={supportLinksData} ns="footer" />
+        </Stack>
+      </Stack>
+    </Section>
+  );
+}
+
+interface LinkItem {
+  url: string;
+  name: string;
+}
+
+const socialLinksData: LinkItem[] = [
+  { url: 'https://github.com/podcodar', name: 'Github' },
+  { url: 'https://www.linkedin.com/company/podcodar/', name: 'LinkedIn' },
+];
+
+const supportLinksData: LinkItem[] = [
+  { url: '/terms-of-service', name: 'terms' },
+  { url: '/privacy-policy', name: 'privacy' },
+];
+
+interface HeaderTitleProps {
+  title: string;
+}
+
+function HeaderTitle({ title }: HeaderTitleProps) {
+  const { t } = useI18n('footer');
+  return (
+    <Text fontWeight="500" fontSize="lg" mb={2}>
+      {t(title)}
+    </Text>
+  );
+}
+
+interface LinksTransalatedProps {
+  ns: TranslationNS;
+  linksData: LinkItem[];
+}
+
+function LinksTransalated({ linksData, ns }: LinksTransalatedProps) {
+  const { t } = useI18n(ns);
+  return (
+    <>
+      {linksData.map((link) => (
+        <Link href={link.url} key={link.name}>
+          {t(link.name)}
+        </Link>
+      ))}
+    </>
   );
 }
--- packages/features/i18n-context.tsx
+++ packages/features/i18n-context.tsx
@@ -81,11 +81,12 @@ export function useI18n(namespace: TranslationNS) {
 
 type Locale = 'en' | 'pt';
 
-type TranslationNS =
+export type TranslationNS =
   | 'common'
   | 'callToAction'
   | 'navbar'
   | 'whyItWorks'
   | 'mentoring'
   | 'roadmap'
+  | 'social-links'
   | 'footer';
