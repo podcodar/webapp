bdeecc55e5e24d4ed7603362dbba382e9a9609ae
joel morais <joel.lasi@hotmail.com>
2021-09-27 18:08:46 -0300

    Criando icons da seção de tenoclogia

---
+++ packages/components/icons/TechIcon.tsx
@@ -0,0 +1,14 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+export const TechIcon = (props: IconProps) => {
+  return (
+    <Icon
+      width="128"
+      height="128"
+      viewBox="0 0 128 128"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    />
+  );
+};
