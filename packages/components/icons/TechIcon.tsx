241faf97fe6291c8fd5a52e41a694ccbd224ca80
joel morais <joel.lasi@hotmail.com>
2021-09-29 18:31:01 -0300

    Adicionando e refatorando icons

--- packages/components/icons/TechIcon.tsx
+++ packages/components/icons/TechIcon.tsx
@@ -3,8 +3,8 @@ import { Icon, IconProps } from '@chakra-ui/react';
 export const TechIcon = (props: IconProps) => {
   return (
     <Icon
-      width="128"
-      height="128"
+      width="64px"
+      height="64px"
       viewBox="0 0 128 128"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
