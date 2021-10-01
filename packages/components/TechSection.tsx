d2e05f72156a2bd4fb3115c4ae48f3868739fca4
Joel Leal <76972953+Joel-leal@users.noreply.github.com>
2021-10-01 10:30:56 -0300

    Criando icons da seção tecnologia e exportando no barril (#42)
    * Criando icons da seção de tenoclogia
    
    * Criando ícone do TS e formatando o ícone JS
    
    * Adicionando e refatorando icons
    
    * Criando componente de SectionTech
    
    * Formatação de texto do CssIcon

---
+++ packages/components/TechSection.tsx
@@ -0,0 +1,64 @@
+import { HStack, Heading } from '@chakra-ui/react';
+
+import { useI18n } from '@packages/features/i18n-context';
+
+import {
+  DockerIcon,
+  GitIcon,
+  JavaScriptIcon,
+  NextIcon,
+  NodeIcon,
+  PythonIcon,
+  ReactIcon,
+  ReduxIcon,
+  ShellIcon,
+  SqlIcon,
+  TypeScriptIcon,
+  LinuxIcon,
+  WebPackIcon,
+  HtmlIcon,
+  CssIcon,
+} from './icons';
+
+const iconList = [
+  GitIcon,
+  HtmlIcon,
+  CssIcon,
+  JavaScriptIcon,
+  ReactIcon,
+  ReduxIcon,
+  TypeScriptIcon,
+  NodeIcon,
+  NextIcon,
+  PythonIcon,
+  ShellIcon,
+  WebPackIcon,
+  SqlIcon,
+  LinuxIcon,
+  DockerIcon,
+];
+
+function TechSection() {
+  const { t } = useI18n('roadmap');
+
+  return (
+    <>
+      <Heading my="4rem" size="md" fontWeight={600} textAlign="left">
+        {t(`tech-title`)}
+      </Heading>
+      <HStack
+        spacing="1rem"
+        flexWrap="wrap"
+        justifyContent="center"
+        alignItems="center"
+        lineHeight="200%"
+      >
+        {iconList.map((Icon) => (
+          <Icon p="0.3rem" key={Icon.name} />
+        ))}
+      </HStack>
+    </>
+  );
+}
+
+export default TechSection;
