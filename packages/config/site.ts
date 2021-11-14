9c7989b71365d4208cfe2a2debac8d141d40c83f
Marco Antônio <ma.souza.junior@gmail.com>
2021-11-14 10:43:17 -0300

    fix: update ux design link
    - remove unecessary comment

--- packages/config/site.ts
+++ packages/config/site.ts
@@ -6,14 +6,12 @@ export const PIX_KEY = 'podcodar@gmail.com';
 // TODO: translate site description
 export const description = 'Democratizando o acesso ao mercado de tecnologia.';
 
-// images
 export const images = {
   icon: '/images/favicon.svg',
   logo: '/images/just-llama.png',
   pixQRCode: '/images/pix-qrcode.png',
 };
 
-//  links
 export const links = {
   // community
   team: '/team',
@@ -30,7 +28,7 @@ export const roadMapsLinks = {
   webProgramming:
     'https://www.notion.so/podcodar/Programa-o-Web-0a244ea5a20f4b73b2c706141f7a4919',
   uxDesign:
-    'https://www.notion.so/podcodar/2dc254c4ea26420d94f400cf1275acd2?v=46a0114a307f4071be615feb0e64879f',
+    'https://www.notion.so/podcodar/UX-Design-f159ae7615d94d99b7a90675f608788a',
   gettingHired:
     'https://www.notion.so/podcodar/Entrar-no-mercado-6132c9e0873948d9943c174f66df7ee3',
   computerScience:
