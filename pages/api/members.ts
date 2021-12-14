13dda1998913d5aea555ba064391018a4e251dd8
Marco <ma.souza.junior@gmail.com>
2021-12-13 21:40:12 -0300

    enh: refactoring api request handlers
    - centralize into a makeRequestHandler utils function
    - add update function

--- pages/api/members.ts
+++ pages/api/members.ts
@@ -1,22 +1,23 @@
 import { getMemberInstance } from '@packages/services/members';
+import { makeRequestHandler } from '@packages/utils/api';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function members(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  const membersDao = getMemberInstance();
+const membersDao = getMemberInstance();
 
-  try {
-    if (req.method === 'POST') {
-      const resp = await membersDao.add(req.body);
-      res.status(200).json(resp);
-    } else {
-      const resp = await membersDao.list();
-      res.status(200).json(resp);
-    }
-  } catch (error) {
-    res.status(403).json(error);
-  }
-}
+const httpVerbHandlers = {
+  GET: async (req: NextApiRequest, res: NextApiResponse) => {
+    const resp = await membersDao.list();
+    return res.status(200).json(resp);
+  },
+  POST: async (req: NextApiRequest, res: NextApiResponse) => {
+    const resp = await membersDao.add(req.body);
+    return res.status(200).json(resp);
+  },
+  PUT: async (req: NextApiRequest, res: NextApiResponse) => {
+    const resp = await membersDao.update(req.body.id, req.body);
+    return res.status(200).json(resp);
+  },
+};
+
+export default makeRequestHandler(httpVerbHandlers);
--- pages/api/questions.ts
+++ pages/api/questions.ts
@@ -1,22 +1,24 @@
 import { getQuestionInstance } from '@packages/services/questions';
+import { makeRequestHandler } from '@packages/utils/api';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function questions(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  const questionDao = getQuestionInstance();
+const questionDao = getQuestionInstance();
 
-  try {
-    if (req.method === 'POST') {
-      const resp = await questionDao.add(req.body);
-      res.status(200).json(resp);
-    } else {
-      const resp = await questionDao.list();
-      res.status(200).json(resp);
-    }
-  } catch (error) {
-    res.status(403).json(error);
-  }
-}
+const httpVerbHandlers = {
+  GET: async (req: NextApiRequest, res: NextApiResponse) => {
+    const resp = await questionDao.list();
+    return res.status(200).json(resp);
+  },
+  POST: async (req: NextApiRequest, res: NextApiResponse) => {
+    const resp = await questionDao.add(req.body);
+    return res.status(200).json(resp);
+  },
+  PUT: async (req: NextApiRequest, res: NextApiResponse) => {
+    const { id, ...body } = req.body;
+    const resp = await questionDao.update(id, body);
+    return res.status(200).json(resp);
+  },
+};
+
+export default makeRequestHandler(httpVerbHandlers);
