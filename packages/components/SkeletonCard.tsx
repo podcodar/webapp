f90b9eda970e8693c406534241537fa2cb450326
Raysson Ornelas de Andrade <86079417+Raysson-ornelas@users.noreply.github.com>
2021-12-23 19:38:34 -0300

    fea: create grid in team page #75 (#101)
    * fea: create grid in team page
    
    * fix: PR suggestions

---
+++ packages/components/SkeletonCard.tsx
@@ -0,0 +1,25 @@
+import {
+  Flex,
+  SkeletonCircle,
+  Box,
+  Skeleton,
+  Grid,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function SkeletonCard() {
+  const colorMode = useColorModeValue('white', 'gray.800');
+  return (
+    <Box w="full" bg={colorMode} boxShadow="xl" rounded="md" pb="2rem">
+      <Skeleton h="8rem" w="full" speed={5} />
+      <Flex justify="center" mt="-12" w="full">
+        <SkeletonCircle size="20" speed={5} />
+      </Flex>
+      <Grid gap="1rem" p="1rem">
+        <Skeleton h="1rem" speed={5} />
+        <Skeleton h="1rem" speed={5} />
+        <Skeleton h="1rem" speed={5} />
+      </Grid>
+    </Box>
+  );
+}
--- pages/team.tsx
+++ pages/team.tsx
@@ -1,9 +1,10 @@
 import { GetStaticProps } from 'next';
-import { Heading, Flex, Text } from '@chakra-ui/react';
+import { Heading, Text, Grid } from '@chakra-ui/react';
 import { Trans } from 'react-i18next';
 
 import { useI18n } from '@packages/features/i18n-context';
 import Section from '@packages/components/Section';
+import SkeletonCard from '@packages/components/SkeletonCard';
 import { Member } from '@packages/entities/members';
 import { getMemberInstance } from '@packages/services/members';
 
@@ -17,11 +18,12 @@ export default function Team({ members, error }: Props) {
   console.log(members, error);
   return (
     <Section py="10rem">
-      <Flex justifyContent="center">
+      <Grid gap={10}>
         <Heading
           fontWeight={600}
           fontSize={{ base: '3xl', sm: '4xl' }}
           lineHeight="110%"
+          textAlign="center"
         >
           <Trans
             i18nKey={t(`title`)}
@@ -30,7 +32,23 @@ export default function Team({ members, error }: Props) {
             }}
           />
         </Heading>
-      </Flex>
+
+        <Grid
+          templateColumns={{
+            base: '1fr',
+            sm: '1fr 1fr',
+            md: 'repeat(3, 1fr)',
+            lg: 'repeat(4, 1fr)',
+          }}
+          gap={{ base: 4, lg: 6 }}
+        >
+          {Array(20)
+            .fill(null)
+            .map((_, index) => (
+              <SkeletonCard key={index} />
+            ))}
+        </Grid>
+      </Grid>
 
       {/* TODO: add member cards here */}
       {/* <div>
