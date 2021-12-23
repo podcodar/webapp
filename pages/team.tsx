0fe372b0d251dca4e6390897cb9bbdf1c99624ef
Raysson Ornelas <rayssonornelas2006@gmail.com>
2021-12-23 19:34:04 -0300

    fix: PR suggestions

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,10 +1,10 @@
 import { GetStaticProps } from 'next';
-import { Heading, Flex, Text, Grid } from '@chakra-ui/react';
+import { Heading, Text, Grid } from '@chakra-ui/react';
 import { Trans } from 'react-i18next';
 
 import { useI18n } from '@packages/features/i18n-context';
 import Section from '@packages/components/Section';
-import FakeCard from '@packages/components/FakeCard';
+import SkeletonCard from '@packages/components/SkeletonCard';
 import { Member } from '@packages/entities/members';
 import { getMemberInstance } from '@packages/services/members';
 
@@ -18,11 +18,12 @@ export default function Team({ members, error }: Props) {
   console.log(members, error);
   return (
     <Section py="10rem">
-      <Flex justifyContent="center">
+      <Grid gap={10}>
         <Heading
           fontWeight={600}
           fontSize={{ base: '3xl', sm: '4xl' }}
           lineHeight="110%"
+          textAlign="center"
         >
           <Trans
             i18nKey={t(`title`)}
@@ -31,24 +32,24 @@ export default function Team({ members, error }: Props) {
             }}
           />
         </Heading>
-      </Flex>
 
-      <Grid
-        templateColumns={{
-          base: '1fr',
-          sm: 'repeat(2, 1fr)',
-          md: 'repeat(3, 1fr)',
-          lg: 'repeat(4, 1fr)',
-        }}
-        gap={{ base: 4, lg: 6 }}
-        mt={10}
-      >
-        {Array(20)
-          .fill(null)
-          .map((_, index) => (
-            <FakeCard key={index} />
-          ))}
+        <Grid
+          templateColumns={{
+            base: '1fr',
+            sm: '1fr 1fr',
+            md: 'repeat(3, 1fr)',
+            lg: 'repeat(4, 1fr)',
+          }}
+          gap={{ base: 4, lg: 6 }}
+        >
+          {Array(20)
+            .fill(null)
+            .map((_, index) => (
+              <SkeletonCard key={index} />
+            ))}
+        </Grid>
       </Grid>
+
       {/* TODO: add member cards here */}
       {/* <div>
         {error != null
