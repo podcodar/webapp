61dda5b3ef243678a5f0544df782b203c09bb231
Marco <ma.souza.junior@gmail.com>
2021-12-30 11:57:37 -0300

    fea: bind initial form states

--- packages/components/AddMemberForm.tsx
+++ packages/components/AddMemberForm.tsx
@@ -5,28 +5,48 @@ import {
   Input,
   Select,
 } from '@chakra-ui/react';
-import { FormEvent } from 'react';
+import { ChangeEvent, FormEvent } from 'react';
 
-function AddMemberForm() {
+import {
+  useAddMemberFormView,
+  useAddMemberFormActions,
+} from '@packages/features/add-member-form-context';
+
+export default function AddMemberForm() {
+  const view = useAddMemberFormView();
+  const { changeGithub } = useAddMemberFormActions();
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
+  const handleGithubChange = (e: ChangeEvent<HTMLInputElement>) => {
+    changeGithub(e.target.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
+      <p>https://api.github.com/users/{view.github}</p>
+
       <FormControl>
         <FormLabel htmlFor="gh-username">Github username</FormLabel>
-        <Input id="gh-username" type="text" />
+        <Input
+          id="gh-username"
+          type="text"
+          value={view.github}
+          onChange={handleGithubChange}
+        />
         <FormHelperText>We will share your github username.</FormHelperText>
       </FormControl>
 
-      <Select placeholder="Select option">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
+      <FormControl>
+        <FormLabel htmlFor="role">Select a role</FormLabel>
+        <Select id="role" placeholder="Select option">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+      </FormControl>
     </form>
   );
 }
-
-export default AddMemberForm;
--- pages/app/add-member.tsx
+++ pages/app/add-member.tsx
@@ -2,12 +2,16 @@ import { Heading } from '@chakra-ui/react';
 
 import AddMemberForm from '@packages/components/AddMemberForm';
 import Section from '@packages/components/Section';
+import { AddMemberFormProvider } from '@packages/features/add-member-form-context';
 
 export default function AddMemberPage() {
   return (
     <Section py="5rem">
       <Heading py="1rem">Add new member</Heading>
-      <AddMemberForm />
+
+      <AddMemberFormProvider>
+        <AddMemberForm />
+      </AddMemberFormProvider>
     </Section>
   );
 }
