7bd1eb987839020a258cb0124085318f9c292edf
vzsoares <jogolots2@gmail.com>
2022-05-12 15:34:37 -0300

    fea:change-language-icons

--- packages/components/ToggleLanguage.tsx
+++ packages/components/ToggleLanguage.tsx
@@ -1,11 +1,17 @@
 import { Button } from '@chakra-ui/react';
 
 import { useI18nActions, useI18nStates } from '@packages/features/i18n-context';
+import { USFlagIcon, BRFlagIcon } from '@packages/components/icons';
 
 export default function ToggleLanguage() {
   const { locale } = useI18nStates();
   const { setLocale } = useI18nActions();
-  const text = locale === 'en' ? '🇧🇷' : '🇺🇸';
+  const text =
+    locale === 'en' ? (
+      <BRFlagIcon style={{ width: '20px' }} />
+    ) : (
+      <USFlagIcon style={{ width: '20px' }} />
+    );
   const handleToggle = () => setLocale(locale === 'en' ? 'pt' : 'en');
 
   return (
