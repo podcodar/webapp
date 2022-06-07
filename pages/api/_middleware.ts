9f35db85595d5a0709b451c046d53343a28b08e5
Marco Antônio <ma.souza.junior@gmail.com>
2022-06-07 11:19:19 -0300

    enh: update dependencies (#130)
    - replace Buffer by atob from middleware as Next Edge doesn't accept it
    - remove useless type definitions

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -5,7 +5,7 @@ export function middleware(req: NextRequest) {
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1];
-    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':');
+    const [user, pwd] = atob(auth).split(':');
 
     if (user === process.env.ADMIN_USER && pwd === process.env.ADMIN_PASSWD) {
       return NextResponse.next();
