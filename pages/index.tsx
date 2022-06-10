83e25a9e8d70620e210b1707556bc205779df854
Guilherme barbosa <guilhermebarbo17@hotmail.com>
2022-06-10 18:16:51 -0300

    enh: refactoring home page sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import WhyItWorksSection from '@packages/components/WhyItWorksSection';
 import MentoringSection from '@packages/components/MentoringSection';
 import RoadmapSection from '@packages/components/RoadmapSection';
 import Footer from '@packages/components/Footer';
+import TestimonialSection from '@packages/components/TestimonialSection';
+import TechSection from '@packages/components/TechSection';
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
       <WhyItWorksSection />
       <MentoringSection />
       <RoadmapSection />
+      <TestimonialSection />
+      <TechSection />
       <Footer />
     </>
   );
