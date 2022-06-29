0c558f0bb474537edc79f40107cff7a925c02c35
vzsoares <jogolots2@gmail.com>
2022-06-28 21:32:39 -0300

    add basic files

---
+++ packages/components/AddTestimonialForm.tsx
@@ -0,0 +1,3 @@
+export default function AddTestimonialForm() {
+  return <div>AddTestimonialForm</div>;
+}
---
+++ pages/app/add-testimonial.tsx
@@ -0,0 +1,5 @@
+import Section from '@packages/components/Section';
+
+export default function addTestimonialPage() {
+  return <Section py="5rem">add-testimonial</Section>;
+}
