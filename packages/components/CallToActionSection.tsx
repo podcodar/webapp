4fc475346d85b3265f8a67fc365d229bdbe5584a
Pedro Frattezi SIlva <pfrattezi@gmail.com>
2024-06-17 08:47:26 -0300

    feat: disable join modal (#149)
    * feat: disable join button and setup informative tooltip
    
    * feat: remove call to action modal usage
    
    * feat: remove join us button in call to action section

--- packages/components/CallToActionSection.tsx
+++ packages/components/CallToActionSection.tsx
@@ -1,7 +1,6 @@
 import { Button, Flex, Heading, Stack, Text } from '@chakra-ui/react';
 import { Trans } from 'react-i18next';
 
-import { useModalActions } from '@packages/features/modal-context';
 import { Illustration } from '@packages/components/icons';
 import { useI18n } from '@packages/features/i18n-context';
 import { links } from '@packages/config/site';
@@ -9,8 +8,8 @@ import { links } from '@packages/config/site';
 import Section from './Section';
 
 export default function CallToActionSection() {
-  const { open } = useModalActions();
   const { t } = useI18n('call-to-action');
+
   return (
     <Section py="10rem">
       <Stack textAlign="center" align="center" spacing={{ base: 8, md: 10 }}>
@@ -26,24 +25,16 @@ export default function CallToActionSection() {
             }}
           />
         </Heading>
+
         <Text color="gray.500" maxW="3xl">
           {t(`description`)}
         </Text>
+
         <Stack
           spacing={6}
           w={{ base: '100%', sm: 'auto' }}
           direction={{ base: 'column', sm: 'row' }}
         >
-          <Button
-            rounded="full"
-            px={6}
-            colorScheme="purple"
-            bg="purple.400"
-            _hover={{ bg: 'purple.500' }}
-            onClick={open}
-          >
-            {t(`main-button`)}
-          </Button>
           <Button as="a" href={links.secondaryButton} rounded="full" px={6}>
             {t(`secondary-button`)}
           </Button>
