35cebde1f8fa59bddaa15094d59d4172afc2d327
Marco Antônio <ma.souza.junior@gmail.com>
2024-06-18 10:45:13 -0300

    fix: next error

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   if (!req.nextUrl.pathname.startsWith('/api')) {
     // This logic is only applied to /about
-    return new Response('Unauthorized', {
-      status: 403,
-      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
-    });
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 403 });
   }
 
   const basicAuth = req.headers.get('authorization');
@@ -19,8 +16,5 @@ export function middleware(req: NextRequest) {
     }
   }
 
-  return new Response('Auth required', {
-    status: 401,
-    headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
-  });
+  return NextResponse.json({ message: 'Auth required' }, { status: 401 });
 }
