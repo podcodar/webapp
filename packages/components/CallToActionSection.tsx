ddf0cddfd0bf4e4e23bad852071fdff5d3bc889e
Diegiwg <diegiwg@gmail.com>
2024-09-23 22:01:47 -0300

    refactor: change Text from chakra to custom Text component

--- packages/components/CallToActionSection.tsx
+++ packages/components/CallToActionSection.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Stack } from "@chakra-ui/react";
 import { Trans } from "react-i18next";
 
 import { Illustration } from "@packages/components/icons";
-import { useI18n } from "@packages/features/i18n-context";
 import { links } from "@packages/config/site";
+import { Text } from "@packages/components/Text";
+import { useI18n } from "@packages/features/i18n-context";
 
 import Section from "./Section";
 
@@ -15,20 +16,26 @@ export default function CallToActionSection() {
   return (
     <Section py="10rem">
       <Stack textAlign="center" align="center" spacing={{ base: 8, md: 10 }}>
-        <Heading fontWeight={600} fontSize={{ base: "3xl", sm: "4xl" }} lineHeight="110%">
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: "3xl", sm: "4xl" }}
+          lineHeight="110%"
+        >
           <Trans
             i18nKey={t("title")}
             components={{
-              span: <Text as="span" color="purple.400" />,
+              span: <Text as="span" style="text-purple-x-500" />,
             }}
           />
         </Heading>
 
-        <Text color="gray.500" maxW="3xl">
-          {t("description")}
-        </Text>
+        <Text style="text-gray-x-500 max-w-3xl">{t("description")}</Text>
 
-        <Stack spacing={6} w={{ base: "100%", sm: "auto" }} direction={{ base: "column", sm: "row" }}>
+        <Stack
+          spacing={6}
+          w={{ base: "100%", sm: "auto" }}
+          direction={{ base: "column", sm: "row" }}
+        >
           <Button as="a" href={links.secondaryButton} rounded="full" px={6}>
             {t("secondary-button")}
           </Button>
