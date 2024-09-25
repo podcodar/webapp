40cbc09c6c85d3d56ec27ebf5ab1dfe33402e1de
Diegiwg <diegiwg@gmail.com>
2024-09-25 15:58:32 -0300

    [fea] setup daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "tailwindcss";
 
+import daisyui from "daisyui";
+
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./packages/**/*.{ts,tsx}"],
+  plugins: [daisyui],
 };
 
 export default config;
