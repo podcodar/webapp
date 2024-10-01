a86b3e2dbb13848077bbde02ecebb12d4ab59219
joel-leal <joel.lasi@hotmail.com>
2024-10-01 19:47:13 -0300

    ref: replace Section padding props with Tailwind classes in PageNotFound

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import Section from "@packages/components/Section";
 
 export default function PageNotFound() {
   return (
-    <Section pb="0" pt="8rem">
+    <Section className="p-0 pt-32">
       <Stack textAlign="center" align="center" spacing={{ base: 2, md: 4 }}>
         <Heading fontWeight={600} fontSize={{ base: "3xl", sm: "4xl" }} lineHeight="110%">
           Não encontramos sua página
