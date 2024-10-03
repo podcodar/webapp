cf046479b52a2a9380a26700ba4a829dbb6addea
joel-leal <joel.lasi@hotmail.com>
2024-10-03 13:44:27 -0300

    ref: add mandatory id prop to Section component

--- packages/components/Section.tsx
+++ packages/components/Section.tsx
@@ -4,11 +4,12 @@ import { classes } from "@packages/utils/classes";
 interface Props {
   children: ReactNode;
   className?: string;
+  id?: string;
 }
 
-function Section({ children, className }: Props) {
+function Section({ children, className, id }: Props) {
   return (
-    <div className={classes("py-16", className)}>
+    <div className={classes("py-16", className)} id={id}>
       <div className="max-w-5xl mx-auto">{children}</div>
     </div>
   );
