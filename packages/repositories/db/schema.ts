f699b4c253e656a987af1f164c5c9e9a8d6ea3ec
Marco Antônio <ma.souza.junior@gmail.com>
2024-10-07 19:01:31 -0300

    ref: cleanup schema

--- packages/repositories/db/schema.ts
+++ packages/repositories/db/schema.ts
@@ -8,22 +8,5 @@ export const testimonialsTable = sqliteTable("testimonials", {
   description: text("description").notNull(),
 });
 
-export type InsertPost = typeof testimonialsTable.$inferInsert;
-export type SelectPost = typeof testimonialsTable.$inferSelect;
-
-//export const postsTable = sqliteTable("posts", {
-//  id: integer("id").primaryKey(),
-//  title: text("title").notNull(),
-//  content: text("content").notNull(),
-//  userId: integer("user_id")
-//    .notNull()
-//    .references(() => usersTable.id, { onDelete: "cascade" }),
-//  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
-//  updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(() => new Date()),
-//});
-//
-//export type InsertUser = typeof usersTable.$inferInsert;
-//export type SelectUser = typeof usersTable.$inferSelect;
-//
-//export type InsertPost = typeof postsTable.$inferInsert;
-//export type SelectPost = typeof postsTable.$inferSelect;
+export type InsertTestimonial = typeof testimonialsTable.$inferInsert;
+export type SelectTestimonial = typeof testimonialsTable.$inferSelect;
