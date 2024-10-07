8310e86d585fd1646473c2538f3ea3cce4b06d20
Marco Antônio <ma.souza.junior@gmail.com>
2024-10-07 19:16:19 -0300

    enh: migrate members

---
+++ scripts/move-members.ts
@@ -0,0 +1,34 @@
+import { db } from "@packages/repositories/db";
+import { membersTable, type InsertMember } from "@packages/repositories/db/schema";
+import { getMemberInstance } from "@packages/services/members";
+
+const firebaseMembers = getMemberInstance();
+
+async function execute() {
+  const membersList = await firebaseMembers.list();
+
+  await Promise.allSettled(
+    membersList.map(async (member) => {
+      const newMember: InsertMember = {
+        name: member.name,
+        role: member.communityRole,
+        avatar: member.images.profile,
+        cover: member.images.cover,
+        github: member.social.github,
+        linkedin: member.social.linkedin,
+      };
+
+      console.log({
+        member,
+        newMember,
+      });
+
+      return db.insert(membersTable).values(newMember);
+    }),
+  );
+}
+
+execute().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
