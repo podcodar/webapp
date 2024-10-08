2ed6a4cde733f0813b0a93a82b37acdacccab55c
Diegiwg <diegiwg@gmail.com>
2024-10-07 21:48:19 -0300

    [ref] improve icon mapping readability

--- packages/components/TechSection.tsx
+++ packages/components/TechSection.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { classes } from "@packages/utils/classes";
 import { useI18n } from "@packages/features/i18n-context";
 
 import {
+  CssIcon,
   DockerIcon,
   GitIcon,
+  HtmlIcon,
   JavaScriptIcon,
+  LinuxIcon,
   NextIcon,
   NodeIcon,
   PythonIcon,
@@ -14,11 +18,9 @@ import {
   ShellIcon,
   SqlIcon,
   TypeScriptIcon,
-  LinuxIcon,
   WebPackIcon,
-  HtmlIcon,
-  CssIcon,
 } from "./icons";
+
 import Section from "./Section";
 
 const iconList = [
@@ -47,9 +49,11 @@ function TechSection() {
       <h2 className="mx-4 my-16 font-semibold text-left text-xl leading-tight">{t("tech-title")}</h2>
 
       <div className="flex flex-wrap mx-4 items-center justify-center">
-        {iconList.map((Icon) => (
-          <Icon className="ml-4:first-child:ml-0" p="0.3rem" key={Icon.name} />
-        ))}
+        {iconList.map((Icon, index) => {
+          const iconSpacing = classes(index > 0 && "ml-4");
+
+          return <Icon className={iconSpacing} style={{ padding: "0.3rem" }} key={Icon.name} />;
+        })}
       </div>
     </Section>
   );
