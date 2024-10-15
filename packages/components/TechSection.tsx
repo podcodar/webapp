a2296f7cd45010b1c4dc2377013c3b0602b075da
Diegiwg <diegiwg@gmail.com>
2024-10-15 10:46:04 -0300

    ref: simplify component icon spacing

--- packages/components/TechSection.tsx
+++ packages/components/TechSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useI18n } from "@packages/features/i18n-context";
-import { classes } from "@packages/utils/classes";
 
 import Section from "./Section";
 import {
@@ -44,15 +43,13 @@ function TechSection() {
   const { t } = useI18n("roadmap");
 
   return (
-    <Section className="py-2">
-      <h2 className="font-semibold text-center text-xl leading-tight p-4 mx-4">{t("tech-title")}</h2>
+    <Section innerClassName="grid gap-8">
+      <h2 className="font-semibold text-center text-xl leading-tight">{t("tech-title")}</h2>
 
-      <div className="flex flex-wrap items-center justify-center p-4 mx-4">
-        {iconList.map((Icon, index) => {
-          const iconSpacing = classes("p-1", index > 0 && "ml-4");
-
-          return <Icon className={iconSpacing} key={Icon.name} />;
-        })}
+      <div className="flex flex-wrap gap-6 items-center justify-center">
+        {iconList.map((Icon) => (
+          <Icon key={Icon.name} />
+        ))}
       </div>
     </Section>
   );
