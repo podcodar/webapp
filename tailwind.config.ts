7b0524bcc8e52606d7ac2c55e5c2953a1f391c37
joel-leal <joel.lasi@hotmail.com>
2024-10-16 11:13:05 -0300

    fix: Removing darkMode configuration change.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./packages/**/*.{ts,tsx}"],
   plugins: [daisyuiPlugin],
-  darkMode: "class",
 };
 
 export default config;
