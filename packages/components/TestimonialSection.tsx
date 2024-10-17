8c3b67443a5423c2b824b7f3d915291175f6d9b2
Marco Antônio <ma.souza.junior@gmail.com>
2024-10-17 16:24:28 -0300

    fea: loop over testimonials

--- packages/components/TestimonialSection.tsx
+++ packages/components/TestimonialSection.tsx
@@ -23,7 +23,14 @@ export default function TestimonialSection({ testimonials }: Props) {
 
       <div className="carousel w-full gap-4">
         {testimonials.map(({ id, name, description, avatarUrl }, idx) => (
-          <TestimonialCard key={id} name={name} testimonial={description} img={avatarUrl} idx={idx} />
+          <TestimonialCard
+            key={id}
+            name={name}
+            testimonial={description}
+            img={avatarUrl}
+            idx={idx}
+            maxSize={testimonials.length - 1}
+          />
         ))}
       </div>
 
@@ -43,9 +50,13 @@ interface TestimonialCardProps {
   testimonial: string;
   img: string;
   idx: number;
+  maxSize: number;
 }
 
-function TestimonialCard({ name, testimonial, img, idx }: TestimonialCardProps) {
+function TestimonialCard({ name, testimonial, img, idx, maxSize }: TestimonialCardProps) {
+  const nextIdx = idx === maxSize ? 0 : idx + 1;
+  const prevIdx = idx === 0 ? maxSize : idx - 1;
+
   return (
     <div className="carousel-item relative w-full border-red-100" id={`testimonial-${idx}`}>
       <div className="card mx-40 min-w-400 border-10 border-blue-500 rounded-sm">
@@ -57,16 +68,16 @@ function TestimonialCard({ name, testimonial, img, idx }: TestimonialCardProps) 
           className="rounded-full border-3 border-blue-500 max-w-4rem sm:max-w-4.5rem object-cover mx-auto"
         />
         <div className="card-body">
-          <h2 className="card-title text-center">{name}</h2>
+          <h2 className="card-title mx-auto">{name}</h2>
           <p>{testimonial}</p>
         </div>
       </div>
 
       <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-        <a href={`#testimonial-${idx - 1}`} className="btn btn-circle">
+        <a href={`#testimonial-${prevIdx}`} className="btn btn-circle">
           ❮
         </a>
-        <a href={`#testimonial-${idx + 1}`} className="btn btn-circle">
+        <a href={`#testimonial-${nextIdx}`} className="btn btn-circle">
           ❯
         </a>
       </div>
