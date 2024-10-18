0d03841d84b8e2950a6bec2a361f78e780e76d11
Marco Antônio <ma.souza.junior@gmail.com>
2024-10-18 16:52:04 -0300

    [fix] move externalLinkIcon (#222)

--- packages/components/Link.tsx
+++ packages/components/Link.tsx
@@ -8,22 +8,9 @@ type Props = LinkProps & {
 
 export default function Link({ children, className, target, href = "", ...props }: Props) {
   if (target) {
-    const externalLinkIcon = (
-      <svg
-        role="img"
-        aria-label="external link icon"
-        className="ml-1 inline h-4 w-4 rotate-[-37deg]"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l6-6m0 0l-6-6m6 6H4" />
-      </svg>
-    );
     return (
       <a target={target} href={href.toString()} className={className} {...props}>
-        {externalLinkIcon} {children}
+        {children} {externalLinkIcon}
       </a>
     );
   }
@@ -34,3 +21,17 @@ export default function Link({ children, className, target, href = "", ...props 
     </NextLink>
   );
 }
+
+const externalLinkIcon = (
+  <svg
+    role="img"
+    aria-label="external link icon"
+    className="w-4 h-4 ml-1 inline rotate-[-37deg]"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l6-6m0 0l-6-6m6 6H4" />
+  </svg>
+);
