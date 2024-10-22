c117bd8e02b4a32ca76b203de3868043141701a7
Diegiwg <diegiwg@gmail.com>
2024-10-21 22:04:30 -0300

    ref: improve to use only one color prop

--- packages/components/RoadmapSection.tsx
+++ packages/components/RoadmapSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useColorModeValue } from "@chakra-ui/react";
+import type { CSSProperties } from "react";
 
 import { roadMapsLinks } from "@packages/config/site";
 import { LocalizedText, type TranslationToken } from "@packages/locale/context";
@@ -8,58 +9,6 @@ import { LocalizedText, type TranslationToken } from "@packages/locale/context";
 import { classes } from "@packages/utils/classes";
 
 import Section from "./Section";
-
-interface CardItemProps {
-  title: TranslationToken;
-  link: string;
-  border: string;
-  shadow: string;
-}
-
-const cardList: CardItemProps[] = [
-  {
-    title: "roadmap.web-programming",
-    link: roadMapsLinks.webProgramming,
-    border: "border-[#17A9BC]",
-    shadow: "hover:shadow-[#17A9BC_-8px_8px_0px]",
-  },
-  {
-    title: "roadmap.ux-design",
-    link: roadMapsLinks.uxDesign,
-    border: "border-[#F99223]",
-    shadow: "hover:shadow-[#F99223_-8px_8px_0px]",
-  },
-  {
-    title: "roadmap.react",
-    link: roadMapsLinks.react,
-    border: "border-[#FF4CFF]",
-    shadow: "hover:shadow-[#FF4CFF_-8px_8px_0px]",
-  },
-  {
-    title: "roadmap.data",
-    link: roadMapsLinks.introToData,
-    border: "border-[#b794f4]",
-    shadow: "hover:shadow-[#b794f4_-8px_8px_0px]",
-  },
-];
-
-function CardItem({ title, link, border, shadow }: Readonly<CardItemProps>) {
-  return (
-    <a href={link} target="_blank" rel="noreferrer">
-      <div
-        className={classes(
-          "flex justify-center py-8 border-2 rounded-lg transition-shadow duration-300 ease-in-out",
-          border,
-          shadow,
-        )}
-      >
-        <h2 className="font-normal text-xl leading-tight">
-          <LocalizedText token={title} />
-        </h2>
-      </div>
-    </a>
-  );
-}
 
 export default function RoadmapSection() {
   const bgColor = useColorModeValue("bg-gray-100", "bg-gray-900");
@@ -77,15 +26,59 @@ export default function RoadmapSection() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {cardList.map((cardProps) => (
-          <CardItem
-            key={cardProps.title}
-            border={cardProps.border}
-            shadow={cardProps.shadow}
-            link={cardProps.link}
-            title={cardProps.title}
-          />
+          <CardItem key={cardProps.title} color={cardProps.color} link={cardProps.link} title={cardProps.title} />
         ))}
       </div>
     </Section>
+  );
+}
+
+interface CardItemProps {
+  title: TranslationToken;
+  link: string;
+  color: string;
+}
+
+const cardList: CardItemProps[] = [
+  {
+    title: "roadmap.web-programming",
+    link: roadMapsLinks.webProgramming,
+    color: "#17A9BC",
+  },
+  {
+    title: "roadmap.ux-design",
+    link: roadMapsLinks.uxDesign,
+    color: "#F99223",
+  },
+  {
+    title: "roadmap.react",
+    link: roadMapsLinks.react,
+    color: "#FF4CFF",
+  },
+  {
+    title: "roadmap.data",
+    link: roadMapsLinks.introToData,
+    color: "#b794f4",
+  },
+];
+
+function CardItem({ title, link, color }: Readonly<CardItemProps>) {
+  const innerStyle = {
+    "--color": color,
+  } as CSSProperties;
+
+  return (
+    <a href={link} target="_blank" rel="noreferrer">
+      <div
+        className={
+          "flex justify-center py-8 border-2 border-[var(--color)] rounded-lg transition-shadow duration-300 ease-in-out hover:shadow-[-8px_8px_0px_0px_var(--color)]"
+        }
+        style={innerStyle}
+      >
+        <h2 className="font-normal text-xl leading-tight">
+          <LocalizedText token={title} />
+        </h2>
+      </div>
+    </a>
   );
 }
