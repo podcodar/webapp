9c73e8f18eada05a0a8744c4c1b12e0bae47f9b9
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-13 11:15:55 -0300

    ref: make toggle theme work with TailwindCSS

--- packages/components/ToggleThemeButton.tsx
+++ packages/components/ToggleThemeButton.tsx
@@ -1,18 +1,40 @@
 "use client";
 
-import { useColorMode } from "@chakra-ui/react";
+import { useState } from "react";
+
+type Theme = "light" | "dark" | "system";
 
 export default function ToggleThemeButton() {
-  const { toggleColorMode, colorMode } = useColorMode();
+  const [colorMode, setColorMode] = useState(document.documentElement.dataset.theme ?? "system");
+
+  function setNewTheme(newColorMode: Theme) {
+    document.documentElement.dataset.theme = newColorMode;
+    document.cookie = `selected-theme=${newColorMode}; path=/`;
+    setColorMode(newColorMode);
+  }
+
+  function handleToggleTheme() {
+    let selectedTheme = document.documentElement.dataset.theme;
+    if (!selectedTheme || selectedTheme === "system") {
+      selectedTheme = window.matchMedia("(prefers-color-scheme: dark)")?.matches ? "dark" : "light";
+    }
+
+    const newColorMode = selectedTheme === "dark" ? "light" : "dark";
+    setNewTheme(newColorMode);
+  }
+
+  const label = `Selected Theme is ${colorMode}. ${colorMode === "system" ? "Click to toggle between light and dark mode." : "Right click to reset to system default."}`;
+
   return (
     <button
       type="button"
       data-testid="toggle-theme"
-      aria-label="Toggle theme button"
-      onClick={toggleColorMode}
+      aria-label={label}
+      onClick={handleToggleTheme}
+      onContextMenu={() => setNewTheme("system")}
       className="btn"
     >
-      {colorMode !== "dark" ? "☀️" : "🌙"}
+      {colorMode === "system" ? "⚙️" : colorMode === "light" ? "☀️" : "🌙"}
     </button>
   );
 }
