0bf265bbaa61e78b26f6c9c98388072806fd8992
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-13 11:16:21 -0300

    fea: setup theme using cookies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import Providers from "@packages/components/Providers";
 
 import "./globals.css";
+import { cookies } from "next/headers";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function RootLayout({ children }: Props) {
+export default async function RootLayout({ children }: Props) {
+  const theme = "system";
+  const cookieStore = cookies();
+  const selectedTheme = cookieStore.get("selected-theme")?.value ?? theme;
+
   return (
-    <html lang="en">
+    <html lang="en" data-theme={selectedTheme}>
       <body>
         <Providers>{children}</Providers>
       </body>
