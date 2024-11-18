e88bee22146960a7fb35eed5528fe49574a5bf87
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-18 13:24:16 -0300

    [ref] remove chakra (#228)

--- packages/components/ToggleThemeButton.tsx
+++ packages/components/ToggleThemeButton.tsx
@@ -1,18 +1,35 @@
 "use client";
 
-import { useColorMode } from "@chakra-ui/react";
+import { type Theme, getSelectedTheme, strToTheme, toggleTheme } from "@packages/utils/theme";
+import { useState } from "react";
 
 export default function ToggleThemeButton() {
-  const { toggleColorMode, colorMode } = useColorMode();
+  const [colorMode, setColorMode] = useState<Theme>(strToTheme(document.documentElement.dataset.theme ?? "system"));
+
+  function setNewTheme(newColorMode: Theme) {
+    document.documentElement.dataset.theme = newColorMode;
+    document.cookie = `selected-theme=${newColorMode}; path=/`;
+    setColorMode(newColorMode);
+  }
+
+  function handleToggleTheme() {
+    const selectedTheme = getSelectedTheme();
+    const newColorMode = toggleTheme(selectedTheme);
+    setNewTheme(newColorMode);
+  }
+
+  const label = `Selected Theme is ${colorMode}. ${colorMode === "system" ? "Click to toggle between light and dark mode." : "Right click to reset to system default."}`;
+
   return (
     <button
       type="button"
       data-testid="toggle-theme"
-      aria-label="Toggle theme button"
-      onClick={toggleColorMode}
+      aria-label={label}
+      onClick={handleToggleTheme}
+      onContextMenu={() => setNewTheme("system")}
       className="btn"
     >
-      {colorMode !== "dark" ? "☀️" : "🌙"}
+      {colorMode === "system" ? "⚙️" : colorMode === "light" ? "☀️" : "🌙"}
     </button>
   );
 }
