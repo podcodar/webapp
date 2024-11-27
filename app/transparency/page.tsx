f8f1d41e808dadd6c22b52000e5b9dd9980ab6a9
joel-leal <joel.lasi@hotmail.com>
2024-11-27 09:47:26 -0300

    Biome correction applied

--- app/transparency/page.tsx
+++ app/transparency/page.tsx
@@ -3,8 +3,8 @@ import Section from "@packages/components/Section";
 function Transparency() {
   return (
     <Section className="px-0 pt-32">
-      <div className="flex flex-col items-center text-center space-y-2 md:space-y-4">
-        <h1 className="font-semibold text-xl sm:text-3xl leading-tight py-30"> PodCodar transparency data </h1>
+      <div className="flex flex-col items-center space-y-2 text-center md:space-y-4">
+        <h1 className=" py-30 font-semibold text-xl leading-tight sm:text-3xl "> PodCodar transparency data </h1>
       </div>
     </Section>
   );
