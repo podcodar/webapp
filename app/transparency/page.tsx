d7228f056f26bc3d7fcf0c1f16d4e3f864ddccad
joel-leal <joel.lasi@hotmail.com>
2024-11-27 01:21:18 -0300

    Route created for transparency data

---
+++ app/transparency/page.tsx
@@ -0,0 +1,13 @@
+import Section from "@packages/components/Section";
+
+function Transparency() {
+  return (
+    <Section className="px-0 pt-32">
+      <div className="flex flex-col items-center text-center space-y-2 md:space-y-4">
+        <h1 className="font-semibold text-xl sm:text-3xl leading-tight py-30"> PodCodar transparency data </h1>
+      </div>
+    </Section>
+  );
+}
+
+export default Transparency;
