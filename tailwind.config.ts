7929415309e10c4fdf58cea47ffb6b458ade7fe3
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-28 11:00:59 -0300

    enh: fix dark mode
    Use dark theme if system is using dark mode or .dark class if the user
    has selected dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,17 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./packages/**/*.{ts,tsx}"],
+  darkMode: [
+    "variant",
+    [
+      // if system selected color is dark
+      "@media (prefers-color-scheme: dark) { &:not(.light *) }",
+      // or if .dark class is present
+      "&:is(.dark *)",
+    ],
+  ],
+
   plugins: [daisyuiPlugin],
-  darkMode: "class",
   daisyui: {
     themes: ["light", "dark"],
   },
