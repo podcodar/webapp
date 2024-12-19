64f99c726113e454430cd278b151a073c9648472
Marco Antônio <ma.souza.junior@gmail.com>
2024-12-19 13:31:53 -0300

    fix: raise issue if env var not found

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   out: "./migrations",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL ?? "missing TURSO_CONNECTION_URL",
-    authToken: process.env.TURSO_AUTH_TOKEN ?? "missing TURSO_AUTH_TOKEN",
+    url: process.env.TURSO_CONNECTION_URL ?? raise("missing TURSO_CONNECTION_URL"),
+    authToken: process.env.TURSO_AUTH_TOKEN ?? raise("missing TURSO_AUTH_TOKEN"),
   },
 });
