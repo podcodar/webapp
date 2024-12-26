e4e15bb8b7dc20327d29a62cf0252db0ab08cd96
Marco Antônio <ma.souza.junior@gmail.com>
2024-12-26 10:20:34 -0300

    fix: add padding to readmap section

--- packages/components/RoadmapSection.tsx
+++ packages/components/RoadmapSection.tsx
@@ -10,7 +10,7 @@ export default function RoadmapSection() {
 	return (
 		<Section
 			className="bg-gray-100 dark:bg-gray-900"
-			innerClassName="grid gap-8"
+			innerClassName="grid gap-8 p-6"
 		>
 			<div className="flex items-center justify-between">
 				<h2 className="font-semibold text-xl leading-tight">
