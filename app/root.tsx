8120b8ccb36caa86d13f41eb8aa9d690c561b225
Marco Antônio <ma.souza.junior@gmail.com>
2025-01-16 08:56:16 -0300

    fix: remove blocking scripts

--- app/root.tsx
+++ app/root.tsx
@@ -20,16 +20,18 @@ import Providers from "@packages/components/Providers";
 import { selectedTheme } from "./cookies.server";
 
 export const links: LinksFunction = () => [
-	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
-	{
-		rel: "preconnect",
-		href: "https://fonts.gstatic.com",
-		crossOrigin: "anonymous",
-	},
-	{
-		rel: "stylesheet",
-		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-	},
+	// NOTE: Example of blocking scripts
+	//
+	//{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+	//{
+	//	rel: "preconnect",
+	//	href: "https://fonts.gstatic.com",
+	//	crossOrigin: "anonymous",
+	//},
+	//{
+	//	rel: "stylesheet",
+	//	href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+	//},
 ];
 
 export async function loader({ request }: LoaderFunctionArgs) {
