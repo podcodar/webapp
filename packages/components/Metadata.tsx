5b3a1d639ac9bc573949df0ad3c2d9caf4cca74f
Marco Antônio <ma.souza.junior@gmail.com>
2025-01-24 11:43:00 -0300

    fix: metatags for og

--- packages/components/Metadata.tsx
+++ packages/components/Metadata.tsx
@@ -1,33 +1,39 @@
+import Head from "next/head";
+
 import { description, images, pageURL, title } from "@packages/config/site";
 
 function Metadata() {
   return (
-    <>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <link rel="icon" type="image/svg+xml" href={images.icon} />
-      <meta name="robots" content="index, follow" />
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" type="image/svg+xml" href={images.icon} />
 
-      {/* Open Graph / Facebook */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={pageURL} />
-      <meta property="og:title" content={title} />
-      <meta property="og:image" content={images.og} />
-      <meta property="og:logo" content={images.og} />
-      <meta property="og:description" content={description} />
+        {/* Google / Search Engine Tags */}
+        <meta itemProp="name" content={title} />
+        <meta itemProp="description" content={description} />
+        <meta itemProp="image" content={images.logo} />
 
-      {/* Google / Search Engine Tags */}
-      <meta itemProp="name" content={title} />
-      <meta itemProp="description" content={description} />
-      <meta itemProp="image" content={images.og} />
+        {/* Facebook Meta Tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={pageURL} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" itemProp="image" content={images.logo} />
+        <meta
+          property="og:image:secure_url"
+          itemProp="image"
+          content={images.logo}
+        />
 
-      {/* Twitter Meta Tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:site" content={pageURL} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={images.og} />
-    </>
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={images.logo} />
+      </Head>
+    </div>
   );
 }
 
