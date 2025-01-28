ac1c2f2c5adc695843a2bf957dbe43acf367e19c
Marco Antônio <ma.souza.junior@gmail.com>
2025-01-28 18:10:57 -0300

    ref: create a factory function for db connection

--- packages/repositories/db/index.ts
+++ packages/repositories/db/index.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@libsql/client";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
+import type { AppLoadContext } from "react-router";
 import { membersTable, testimonialsTable } from "./schema";
 
 config({ path: ".env" }); // or .env.local
@@ -23,4 +24,11 @@ export class Database {
   get members() {
     return this.db.select().from(membersTable);
   }
+}
+
+export function getDatabase(context: AppLoadContext) {
+  return new Database(
+    context.cloudflare.env.TURSO_CONNECTION_URL,
+    context.cloudflare.env.TURSO_AUTH_TOKEN,
+  );
 }
