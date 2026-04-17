32e35968e15ad77809bb111374d8a7b015d1a43c
Marco Souza <marco.souza@mongodb.com>
2026-04-17 10:18:03 -0300

    fix: use podcodar favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PodCodar';
+export const SITE_DESCRIPTION = 'Welcome to PodCodar! We believe that knowledge should be accessible to everyone, and that learning should be fun and engaging. Our mission is to provide high-quality educational content that empowers individuals to learn new skills and explore new ideas. Whether you are a student, a professional, or just someone who loves to learn, we have something for you. Join us on this journey of discovery and growth!';
