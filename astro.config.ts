0c8993ab6aba5f3e3bd5aeabcd0e77bd5bc07c0b
Marco <ma.souza.junior@gmail.com>
2026-04-20 09:58:35 -0300

    fix: remove unit tests

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   site: process.env.BASE_URL,
-  adapter: cloudflare({ prerenderEnvironment: 'node' }),
+  adapter: cloudflare(),
 
   i18n: {
     locales: ['pt-br'],
