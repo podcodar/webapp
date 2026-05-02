bd1e3b81a973e9ce247bc0ad41d8000f1552e75c
Marco Souza <marco.souza@mongodb.com>
2026-05-02 07:34:08 -0300

    fix: update rss to xml

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
 import { SITE_DESCRIPTION, SITE_TITLE } from '@/consts';
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
   const posts = await getCollection('blog');
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
+    site: context.url,
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.id}/`,
